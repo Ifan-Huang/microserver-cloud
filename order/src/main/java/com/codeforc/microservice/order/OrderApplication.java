b331fdbb1fc2be72d8ac837053ea5d4542ed2278
IfanHuang <ifanhuang@aliyun.com>
2021-12-24 18:24:43 +0800

    update application.yml for registry.

--- order/src/main/java/com/codeforc/microservice/order/OrderApplication.java
+++ order/src/main/java/com/codeforc/microservice/order/OrderApplication.java
@@ -2,11 +2,13 @@ package com.codeforc.microservice.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Author: Ifan·Huang
  * Description: 订单服务主程序
  **/
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderApplication {
 
--- payment/src/main/java/com/codeforc/microservice/payment/PaymentApplication.java
+++ payment/src/main/java/com/codeforc/microservice/payment/PaymentApplication.java
@@ -2,11 +2,13 @@ package com.codeforc.microservice.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Author: Ifan·Huang
  * Description: 支付服务主程序启动类,该服务旨在为项目提供稳定精确的支付服务。
  **/
+@EnableEurekaClient
 @SpringBootApplication
 public class PaymentApplication {
 
